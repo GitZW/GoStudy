fc4ef348e82b151cd2a23eeeafdd906248666cbd
zhangwei <474268013@qq.com>
2024-07-23 08:19:08 +0800

    day13

--- day12/day12.go
+++ day12/day12.go
@@ -1,18 +1,40 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// SafeCounter 是并发安全的
+type SafeCounter struct {
+	mu sync.Mutex
+	v  map[string]int
+}
+
+// Inc 对给定键的计数加一
+func (c *SafeCounter) Inc(key string, index int) {
+	c.mu.Lock()
+	println(index)
+	// 锁定使得一次只有一个 Go 协程可以访问映射 c.v。
+	c.v[key]++
+	c.mu.Unlock()
+}
+
+// Value 返回给定键的计数的当前值。
+func (c *SafeCounter) Value(key string) int {
+	c.mu.Lock()
+	// 锁定使得一次只有一个 Go 协程可以访问映射 c.v。
+	defer c.mu.Unlock()
+	return c.v[key]
+}
+
 func main() {
-	ch1 := make(chan int, 2)
-	ch1 <- 1
-	ch1 <- 2
-
-	ch2 := make(chan int, 2)
-	ch2 <- 1
-	ch2 <- 3
-
-	if <-ch1 == <-ch2 {
-		println("true")
-	} else {
-		println("false")
+	c := SafeCounter{v: make(map[string]int)}
+	for i := 0; i < 100; i++ {
+		go c.Inc("somekey", i)
 	}
 
+	time.Sleep(time.Second)
+	fmt.Println(c.Value("somekey"))
 }
